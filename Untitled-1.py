f7f71acd5e6f58c3e2ffda8ed7c15e08bbb17acc
MrVansic <167616923+MrVansic@users.noreply.github.com>
2024-05-11 13:07:28 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -6,6 +6,10 @@ back = (133,122,222)
 window.fill(back)
 game = True 
 finish = False
+font.init()
+font = font.Font(None,70)
+lose1 = font.render('You lose',True,(44,55,66))
+lose2 = font.render('You lose',True,(44,55,66))
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image,player_x,player_y,size_x,size_y,player_speed):
         super().__init__()
@@ -33,21 +37,40 @@ class Player(GameSprite):
             self.rect.y -= self.player_speed
         if keys[K_s] and self.rect.y < (700 - 5 - self.size_y):
             self.rect.y += self.player_speed
-player = Player('ракетка.jpg',10,440,50,50,5)
-player1 = Player('ракетка.jpg',10,440,50,50,5)
-sprite = GameSprite('product_14.png',10,440,50,50,5)
+player = Player('ракетка.png',620,440,50,100,5)
+player1 = Player('ракетка.png',10,440,50,100,5)
+ball = GameSprite('product_14.png',250,220,30,30,5)
+speed_x = 3
+speed_y = 3
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     if not finish:
         window.fill(back)
-        player.reset()
-        player.updatel()
-        player1.reset()
-        player1.updater()
-        sprite.reset()
         
+        player.updatel()
+       
+        player1.updater()
+        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(player,ball) or sprite.collide_rect(player1,ball):
+            speed_y *= -1
+            speed_x *= -1
+        if ball.rect.y > 500-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(200,200))
+            game = True
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose2,(200,200))
+            game = True
+        player.reset()
+        player1.reset()
+        ball.reset()
         display.update()
     time.delay(50)
 
